0c674e761caa26fb82ba4a961ae490e12d5c60a6
Mário <mario@dindigital.com>
2021-07-31 21:12:23 -0300

    Episode 2 - Initial Setup and Design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full border-none rounded-xl px-4 py-2">
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full border-none rounded-xl px-4 py-2">
+                <option value="1">Other Filter 1</option>
+                <option value="2">Other Filter 2</option>
+            </select>
+        </div>
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an idea" class="w-full placeholder-gray-900 rounded-xl bg-white px-4 py-2 pl-8 border-none">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
