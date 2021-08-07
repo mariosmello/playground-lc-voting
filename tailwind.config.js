5c6e69e1d056043d7ffda240c6abaa644458a533
Mário <mario@dindigital.com>
2021-08-06 21:04:56 -0300

    Atualizando etapa de frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -12,29 +13,38 @@ module.exports = {
             colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
+
                 black: colors.black,
                 white: colors.white,
                 gray: colors.trueGray,
                 'gray-background': '#f7f8fc',
                 'blue': '#328af1',
                 'blue-hover': '#2879bd',
-                'yellow': '#ffc73c',
-                'red': '#ec454f',
-                'green': '#1aab8b',
-                'purple': '#8b60ed',
-            },
-            maxWidth: {
-                custom: '62.5rem',
+                'yellow' : '#ffc73c',
+                'red' : '#ec454f',
+                'green' : '#1aab8b',
+                'purple' : '#8b60ed',
             },
             spacing: {
+                22: '5.5rem',
+                44: '11rem',
                 70: '17.5rem',
+                76: '19rem',
+                104: '26rem',
                 175: '43.75rem',
+            },
+            maxWidth: {
+                custom: '68.5rem',
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
             },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
-                xxs: ['0.625rem', {lineHeight: '1rem'}],
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
             },
         },
     },
